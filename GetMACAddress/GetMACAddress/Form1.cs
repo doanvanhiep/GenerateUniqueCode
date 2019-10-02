4d4d4ecf43b52f28156835d4565e647651d03926
unknown <doanvanhiep>
2019-10-02 12:01:03 +0700

    generate unique code

---
+++ GetMACAddress/GetMACAddress/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetMACAddress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = FingerPrint.Value().ToString();
+            //return;
+            //string a = "";
+            //foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            //{
+            //    // Only consider Ethernet network interfaces
+            //    if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+            //        nic.OperationalStatus == OperationalStatus.Up)
+            //    {
+            //        textBox1.Text= nic.GetPhysicalAddress().ToString();
+            //        return;
+            //    }
+            //}
+
+        }
+    }
+}
